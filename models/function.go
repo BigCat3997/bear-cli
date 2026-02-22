2b299de76165783e736ee0c6f3d81e075a722da3
bigcat3997 <vqt.develop@gmail.com>
2026-02-22 16:18:54 +0700

    rename to PsLoginByCredential

--- models/function.go
+++ models/function.go
@@ -3,17 +3,17 @@ package models
 type Command string
 
 const (
-	Ps                 Command = "ps"
-	PsCreateCredential Command = "create-cred"
-	PsGetCredential    Command = "get-cred"
-	PsInitCredential   Command = "init-cred"
-	PsLoginByCred      Command = "login-by-cred"
+	Ps                  Command = "ps"
+	PsCreateCredential  Command = "create-cred"
+	PsGetCredential     Command = "get-cred"
+	PsInitCredential    Command = "init-cred"
+	PsLoginByCredential Command = "login-by-cred"
 )
 
 var CommandDescriptions = map[Command]string{
-	Ps:                 "Interact with PluralSight resources",
-	PsCreateCredential: "Creates credential",
-	PsGetCredential:    "Get credential",
-	PsInitCredential:   "Initialize credential in every consumed environment (e.g. Terraform's variables.tf, etc.)",
-	PsLoginByCred:      "Log in to appropriate cloud by credential",
+	Ps:                  "Interact with PluralSight resources",
+	PsCreateCredential:  "Creates credential",
+	PsGetCredential:     "Get credential",
+	PsInitCredential:    "Initialize credential in every consumed environment (e.g. Terraform's variables.tf, etc.)",
+	PsLoginByCredential: "Log in to appropriate cloud by credential",
 }
